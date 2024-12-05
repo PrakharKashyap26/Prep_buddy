55a80e46277c097ffff84b49a585f23298ca1912
Prakhar kashyap <prakharkashyap2607@gmail.com>
2024-12-05 21:58:51 +0530

    Course_search.py

--- course_search.py
+++ course_search.py
@@ -3,8 +3,8 @@ import requests
 def recommend_resources():
     skill = input("Enter your skill (e.g., python, data science, web development): ").strip().lower()
     
-    api_key = 'AIzaSyC32bZZJZCcCzxYQ78dP2sDX-9-S2Fjmuw'  # Replace with your API key
-    cx = '46a1526f67d934c6c'  # Replace with your Custom Search Engine ID
+    api_key = 'api-key-'  # Replace with your API key
+    cx = 'id'  # Replace with your Custom Search Engine ID
     url = 'https://www.googleapis.com/customsearch/v1'
     params = {'key': api_key, 'cx': cx, 'q': f"online courses {skill}"}
 
